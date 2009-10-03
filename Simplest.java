4711d5955727887a7828e085d8bb8ec7d81672e0
Anil Wadghule <anildigital@gmail.com>
2009-10-04 00:59:55 +0530

    Simplest applet

---
+++ Simplest.java
@@ -0,0 +1,8 @@
+public class Simplest extends java.applet.Applet {
+  public void paint(java.awt.Graphics qr) {
+    qr.drawString("Seeing is beleiving", 25, 75);
+    qr.drawRect(0, 0, 299, 199);
+  }
+}
+
+
